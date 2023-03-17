9e089d55e63cfccd994df6ad75d390820ee620a4
curlew <git@curlew.is>
2023-03-18 06:30:26 +1000

    automatic role assignment on member join

--- bot/ext/moderation.py
+++ bot/ext/moderation.py
@@ -1,10 +1,51 @@
-from typing import Union
+from typing import Union, Literal
 import discord
 from discord.ext import commands
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        guild = member.guild
+        autorole_id = await self.bot.db_pool.fetchval("SELECT autorole_id FROM guilds WHERE id = $1", guild.id)
+        if autorole_id is not None:
+            autorole = discord.utils.get(guild.roles, id=autorole_id)
+            await member.add_roles(autorole)
+
+    @commands.command(help="Sets a role to be automatically assigned to new members")
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.bot_has_permissions(manage_roles=True)
+    async def autorole(self, ctx,
+                       option: Literal["enable", "disable", "status"]
+                       = commands.param(description="enable/disable/status"),
+                       role: discord.Role = None):
+        e = discord.Embed(color=discord.Color.green())
+
+        if option == "enable":
+            if role is None:
+                raise commands.MissingRequiredArgument(self.autorole.params["role"])
+            query = """INSERT INTO guilds (id, autorole_id) VALUES ($1, $2)
+                       ON CONFLICT (id) DO UPDATE SET autorole_id = EXCLUDED.autorole_id"""
+            await self.bot.db_pool.execute(query, ctx.guild.id, role.id)
+            e.title = f"Set autorole to *{role.name}*"
+        elif option == "disable":
+            query = """INSERT INTO guilds (id, autorole_id) VALUES ($1, NULL)
+                       ON CONFLICT (id) DO UPDATE SET autorole_id = NULL"""
+            await self.bot.db_pool.execute(query, ctx.guild.id)
+            e.title = "Disabled autorole"
+        elif option == "status":
+            query = "SELECT autorole_id FROM guilds WHERE id = $1"
+            current_autorole_id = await self.bot.db_pool.fetchval(query, ctx.guild.id)
+            if current_autorole_id is None:
+                e.title = f"Autorole is currently disabled"
+            else:
+                current_autorole = discord.utils.get(ctx.guild.roles, id=current_autorole_id)
+                e.title = f"The current autorole is *{current_autorole}*"
+
+        await ctx.channel.send(embed=e)
 
     @commands.command(help="Kicks a user")
     @commands.has_permissions(kick_members=True)
